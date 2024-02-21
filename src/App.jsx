fb2f1494166cf3441324733a34792e18345645d0
David Rivera A <davidriveraarbelaez@gmail.com>
2024-02-21 12:34:44 -0500

    table-example

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Table from './Components/Table.jsx';
+
+function App() {
+
+  return (
+    <>
+    <h1 className="bg-red-200" > Hola clase </h1>
+    <button className="bg-sky-400 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded" onClick={()=> window.open('https://tailwindcss.com/', '_blank')} > Help me</button>
+ 
+<div class="grid grid-cols-10 gap-2">
+  <div class="bg-violet-50 aspect-square"></div>
+  <div class="bg-violet-100 aspect-square"></div>
+  <div class="bg-violet-200 aspect-square"></div>
+  <div class="bg-violet-300 aspect-square"></div>
+  <div class="bg-violet-400 aspect-square"></div>
+  <div class="bg-violet-500 aspect-square"></div>
+  <div class="bg-violet-600 aspect-square"></div>
+  <div class="bg-violet-700 aspect-square"></div>
+  <div class="bg-violet-800 aspect-square"></div>
+  <div class="bg-violet-900 aspect-square"></div>
+</div>
+
+<Table />
+
+
+
+    </>
+  )
+}
+
+export default App
